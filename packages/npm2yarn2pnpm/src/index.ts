2e75537a99d0614f55c4e3c8c963d0a37cf02768
Jeroen Claassens <support@favware.tech>
2021-11-26 01:03:39 +0100

    fix(npm2yarn2pnpm): removed esm exports to ensure cjs works fine with require syntax

--- packages/npm2yarn2pnpm/src/index.ts
+++ packages/npm2yarn2pnpm/src/index.ts
@@ -1,2 +1,7 @@
+import { npm2yarn2pnpm } from './npm2yarn2pnpm';
+
 export { npmToPnpm } from './npm2pnpm';
-export { npm2yarn2pnpm as default, npm2yarn2pnpm, PluginOptions } from './npm2yarn2pnpm';
+export { npm2yarn2pnpm, PluginOptions } from './npm2yarn2pnpm';
+
+module.exports = npm2yarn2pnpm;
+export default npm2yarn2pnpm;
