dfab541b725a5296eb6768c85365f1f1ea3e0b87
Jeroen Claassens <support@favware.tech>
2022-07-12 00:17:39 +0200

    test: add unit tests

---
+++ packages/npm2yarn2pnpm/tests/npm2pnpm.test.ts
@@ -0,0 +1,16 @@
+import { npmToPnpm } from '../src/npm2pnpm';
+
+describe('npm2pnpm', () => {
+	const commandPairs: [npm: string, pnpm: string][] = [
+		// ['npm install pkg', 'pnpm add pkg'],
+		['npm install pkg --save-dev', 'pnpm add pkg --save-dev'],
+		['npm install pkg --save', 'pnpm add pkg --save-prod'],
+		['npm install pkg --save-exact', 'pnpm add pkg --save-exact'],
+		['npm install pkg --global', 'pnpm add pkg --global'],
+		['npm install pkg --ignore-scripts', 'pnpm add pkg --ignore-scripts']
+	];
+
+	test.each(commandPairs)('GIVEN %s THEN transforms to %s', (npmCommand, pnpmCommand) => {
+		expect(npmToPnpm(npmCommand)).toEqual(pnpmCommand);
+	});
+});
---
+++ packages/npm2yarn2pnpm/tests/npm2yarn.test.ts
@@ -0,0 +1,17 @@
+import npmToYarn from '@armano/npm-to-yarn';
+
+describe('npm2yarn', () => {
+	const commandPairs: [npm: string, yarn: string][] = [
+		['npm install pkg', 'yarn add pkg'],
+		['npm install pkg --save-dev', 'yarn add pkg --dev'],
+		['npm install pkg --save', 'yarn add pkg'],
+		// Incorrect, when https://github.com/armano2/npm-to-yarn/issues/7 is fixed this will make the test automatically fail
+		['npm install pkg --save-exact', 'yarn add pkg --exact'],
+		['npm install pkg --global', 'yarn global add pkg'],
+		['npm install pkg --ignore-scripts', 'yarn add pkg --ignore-scripts']
+	];
+
+	test.each(commandPairs)('GIVEN %s THEN transforms to %s', (npmCommand, yarnCommand) => {
+		expect(npmToYarn(npmCommand, 'yarn')).toEqual(yarnCommand);
+	});
+});
