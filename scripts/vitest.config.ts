2a7871fe13611704da5610a27d2959c9f59d78d9
Jeroen Claassens <support@favware.tech>
2022-07-12 00:11:44 +0200

    chore: setup unit testing

---
+++ scripts/vitest.config.ts
@@ -0,0 +1,15 @@
+import type { ESBuildOptions } from 'vite';
+import { defineConfig, type UserConfig } from 'vitest/config';
+
+export const createVitestConfig = (options: UserConfig = {}) =>
+	defineConfig({
+		...options,
+		test: {
+			...options?.test,
+			globals: true
+		},
+		esbuild: {
+			...options?.esbuild,
+			target: (options?.esbuild as ESBuildOptions | undefined)?.target ?? 'es2021'
+		}
+	});
