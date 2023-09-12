e589933fa2940929eb5763d6b1415343e3ef2dfa
tawfik boujeh <tboujeh@gmail.com>
2023-09-13 00:47:47 +0200

    fix: export `npmToBun` in `index.ts`

--- packages/npm2yarn2pnpm/src/index.ts
+++ packages/npm2yarn2pnpm/src/index.ts
@@ -1,3 +1,4 @@
 export { npmToPnpm } from './npm2pnpm';
 export { npmToYarn } from './npm2yarn';
+export { npmToBun } from './npm2bun';
 export { npm2yarn2pnpm, type PluginOptions } from './npm2yarn2pnpm';
