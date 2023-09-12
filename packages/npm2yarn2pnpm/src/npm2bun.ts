03461cd0541e6b1f8cc9567b7ab8c1a39299328d
tawfik boujeh <tboujeh@gmail.com>
2023-09-13 00:17:06 +0200

    chore: remove copyright banner

--- packages/npm2yarn2pnpm/src/npm2bun.ts
+++ packages/npm2yarn2pnpm/src/npm2bun.ts
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) 2019 Ben Gubler <nebrelbug@gmail.com>
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 const unchangedCLICommands = ['init', 'run', 'test', 'login', 'logout', 'link', 'publish', 'cache'];
 
 function parseNpmInstall(command: string, isShortHand = false) {
