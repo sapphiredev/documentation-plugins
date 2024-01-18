50992b91b2ad1ff7efc58fbe39a1d7cd75898a50
Jeroen Claassens <support@favware.tech>
2024-01-18 21:28:28 +0100

    fix: properly split CJS and ESM

--- packages/typedoc-djs-links/tsup.config.ts
+++ packages/typedoc-djs-links/tsup.config.ts
@@ -1,12 +1,28 @@
-import { defineConfig } from 'tsup';
+import { Options, defineConfig } from 'tsup';
 
-export default defineConfig({
+const baseOptions: Options = {
 	clean: true,
 	dts: true,
 	entryPoints: ['src/index.ts'],
-	format: ['esm', 'cjs'],
 	minify: false,
 	skipNodeModulesBundle: true,
 	sourcemap: true,
-	target: 'es2021'
-});
+	target: 'es2021',
+	tsconfig: 'src/tsconfig.json',
+	keepNames: true,
+	treeshake: true
+};
+
+export default [
+	defineConfig({
+		...baseOptions,
+		outDir: 'dist/cjs',
+		format: 'cjs',
+		outExtension: () => ({ js: '.cjs' })
+	}),
+	defineConfig({
+		...baseOptions,
+		outDir: 'dist/esm',
+		format: 'esm'
+	})
+];
