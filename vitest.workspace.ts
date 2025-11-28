06136f7ac0e4194e7da358b744d0358fd675662b
Jeroen Claassens <jeroen.claassens@rws.nl>
2025-11-28 12:48:01 +0100

    chore: increased loading time by a bit

--- vitest.workspace.ts
+++
@@ -1,7 +0,0 @@
-import { defineWorkspace } from 'vitest/config';
-
-export default defineWorkspace([
-	'./packages/npm2yarn2pnpm/vitest.config.ts',
-	'./packages/typedoc-djs-links/vitest.config.ts',
-	'./packages/ts2esm2cjs/vitest.config.ts'
-]);
